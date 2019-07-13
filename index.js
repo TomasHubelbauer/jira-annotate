5d48a920b474b188d7ad84f4364a867608752758
Tomáš Hübelbauer <tomas@hubelbauer.net>
2019-07-13 08:36:03 +0200

    Simplify the code

--- index.js
+++ index.js
@@ -1,33 +1,12 @@
 window.addEventListener('load', async event => {
-  const config = await fetchConfig();
-  const myself = await fetchApi(config, 'GET', '/rest/api/2/myself')
-
-
+  const configResponse = await fetch('config.json');
+  const config = await configResponse.json();
+  const myself = await fetchJiraApi(config, 'GET', '/rest/api/2/myself')
+  console.log(myself);
 });
 
-async function logError(error) {
-  console.groupCollapsed(error.message);
-  console.log(error.stack);
-  console.groupEnd();
-}
-
-async function fetchConfig() {
-  try {
-    const response = await fetch('config.json');
-    return await response.json();
-  } catch (e) {
-    logError(e);
-    return null;
-  }
-}
-
-async function fetchApi(config, method, endpoint) {
-  try {
-    const headers = { Authorization: `Basic ${window.btoa(config.userName + ':' + config.passphrase)}` };
-    const response = await fetch(config.hostName + endpoint, { method, headers });
-    return await response.json();
-  } catch (e) {
-    logError(e);
-    return null;
-  }
+async function fetchJiraApi(config, method, endpoint) {
+  const headers = { Authorization: `Basic ${window.btoa(config.userName + ':' + config.passphrase)}` };
+  const response = await fetch(config.hostName + endpoint, { method, headers });
+  return await response.json();
 }
